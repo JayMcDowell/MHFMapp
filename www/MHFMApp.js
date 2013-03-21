05e0864ce15423c3b382f86252551b75711d5996
JayMcDowell <jay@musicianshalloffame.com>
2013-03-20 22:58:15 -0500

    phonegap implementation

---
+++ www/MHFMApp.js
@@ -0,0 +1,15 @@
+	$(document).ready(function() {
+		
+	}); //end of doc ready
+	function playStream() {
+  try {
+    var myaudio = new Audio('http://neon.wavestreamer.com:8047');
+    myaudio.id = 'playerMyAdio';
+    myaudio.play();
+  } catch (e) {
+    alert('no audio support!');
+  } 
+}
+
+
+
