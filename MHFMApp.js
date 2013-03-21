5cc68536b990fa03386e9c7a8a68a2d15496c64a
JayMcDowell <jay@musicianshalloffame.com>
2013-03-20 23:24:48 -0500

    test

--- MHFMApp.js
+++
@@ -1,6 +0,0 @@
-	$(document).ready(function() {
-  		$('.holder').hide();
-	$('h1').click(function() {
-  			$(this).toggleClass('close').next().slideToggle("slow");
-  		});//end of hide
-	}); //end of doc ready
