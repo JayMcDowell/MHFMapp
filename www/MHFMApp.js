5e30cafcd1a6a61e2ae9268aa82d423991f4a77f
JayMcDowell <jay@musicianshalloffame.com>
2013-03-23 15:58:32 -0500

    implemented vidfit

--- www/MHFMApp.js
+++ www/MHFMApp.js
@@ -1,7 +1,7 @@
 	$(document).ready(function() {
-		 $("#plyr").fitVids();
-	}); //end of doc ready
-	function playStream() {
+		 $(".video-container").fitVids();
+  }); //end of doc ready
+  function playStream() {
   try {
     var myaudio = new Audio('http://neon.wavestreamer.com:8047');
     myaudio.id = 'playerMyAdio';
